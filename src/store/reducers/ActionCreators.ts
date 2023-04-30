254b103905a7c6ed591c460f9282fd08fc40436d
Serhii <grammas56@gmail.com>
2023-04-30 18:13:09 +0300

    createAsyncThunk extraReducers axios

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,16 +1,17 @@
 import axios from "axios";
-import { AppDispatch } from "../store";
 import { IUser } from "../../models/IUser";
-import {userSlice} from './UserSlice';
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 
-export const fetchUsers = () => async (dispatch:AppDispatch)=>{
-    try{
-        dispatch(userSlice.actions.usersFetching())
-        const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users')
-        dispatch(userSlice.actions.usersFetchingSuccess([response.data]))
 
-    } catch {
-        dispatch(userSlice.actions.usersFetchingError('Error'))
+export const fetchUsers = createAsyncThunk(
+    'user/fetchAll',
+    async(_, thunkAPI)=>{
+        try{
+            const response = await axios.get<IUser>('https://jsonplaceholder.typicode.com/users')
+            return response.data;
+        }catch {
+            return thunkAPI.rejectWithValue('Error')
+        }
     }
-} 
+)
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 import { IUser } from "../../models/IUser"
+import { fetchUsers } from "./ActionCreators";
 
 interface UserState {
     users: IUser[];
@@ -19,18 +20,19 @@ const initialState: UserState = {
 export const userSlice = createSlice({
     name: 'user',
     initialState,
-    reducers: {
-        usersFetching(state){
-            state.isLoading = true;
-        },
-        usersFetchingSuccess(state,action:PayloadAction<IUser[]>){
+    reducers: {},
+    extraReducers: {
+        [fetchUsers.fulfilled.type](state, action:PayloadAction<IUser[]>){
             state.isLoading = false;
             state.error = '';
             state.users = action.payload; 
         },
-        usersFetchingError(state,action:PayloadAction<string>){
+        [fetchUsers.pending.type](state){
+            state.isLoading = true;
+        },
+        [fetchUsers.rejected.type](state,action:PayloadAction<string>){
             state.isLoading = false;
-            state.error = action.payload
+            state.error = action.payload;
         },
     }
 })
